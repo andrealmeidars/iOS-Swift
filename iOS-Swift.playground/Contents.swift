f56efbe26b0aad648b8b0911b143d42a24b226bd
ricardorauber <ricardorauber@gmail.com>
2015-10-06 20:09:13 -0300

    Created the Playground

---
+++ iOS-Swift.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: ## iOS Development in Swift 2
+//: Created by Ricardo Rauber - 2015
+//:
+//: ----
+//:
+//: The latest version of this Playground can be found in my GitHub:
+//: - https://github.com/ricardorauber/iOS-Swift
+//:
+//: ----
+//:
+//: Use the Project Navigator to go through the contents of this Playground.
+
+print("Welcome to Swift!")
